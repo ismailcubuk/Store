43fdc7b8dafa9ec552d028a8652ca391ebf16c38
ismailcubuk <iismailcubuk@gmail.com>
2023-05-30 13:24:39 +0300

    XİAOMİ POCO X5 8GB/256GB

--- src/api/products.js
+++ src/api/products.js
@@ -774,6 +774,42 @@ const products = {
         },
       ],
     },
+    {
+      id: 11,
+      tittle: "XİAOMİ POCO X5 8GB/256GB",
+      category: "Phone",
+      description: "XİAOMİ POCO X5 8GB/256GB",
+      brand: "XİAOMİ",
+      color: ["black"],
+      memory: 256,
+      rating: {
+        stars: 3,
+        count: 42,
+      },
+      features: {
+        ProcessorSpeed: 2.2,
+        NumberofProcessors: 8,
+        display: "6.5",
+        processor: "ARM Cortex-A78",
+        ram: 8,
+        camera: 48,
+      },
+      price: {
+        quantity: 12.999,
+        currency: "TRY",
+      },
+      images: [
+        {
+          variant: "black",
+          images: [
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
+          ],
+        },
+      ],
+    },
   ],
 };
 
