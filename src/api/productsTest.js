61f3ae8fb7cbed46499cdade5adeb151b6665dd7
ismailcubuk <iismailcubuk@gmail.com>
2023-05-30 16:54:21 +0300

    Samsung Galaxy Z Fold 4

--- src/api/productsTest.js
+++ src/api/productsTest.js
@@ -2,80 +2,26 @@ const products = {
   phones: [
     {
       id: 11,
-      tittle: "Samsung Galaxy S23 Ultra 256 GB",
+      tittle: "Samsung Galaxy Z Fold 4",
       category: "Phone",
-      description: "Samsung Galaxy S23 Ultra 256 GB",
+      description: "Samsung Galaxy Z Fold 4",
       brand: "SAMSUNG",
       color: ["white", "black", "green"],
-      memory: [256, 512],
+      memory: [256],
       rating: {
         stars: 5,
-        count: 11,
+        count: 7,
       },
       features: {
-        ProcessorSpeed: 3.36,
+        ProcessorSpeed: 3.32,
         NumberofProcessors: 8,
-        display: 6.8,
-        processor: "Qualcomm SM8550",
-        ram: 8,
-        camera: 200,
+        display: 6.2,
+        processor: "Qualcomm SM8475",
+        ram: 12,
+        camera: 50,
       },
       price: {
         quantity: 38.499,
-        currency: "TRY",
-      },
-      images: [
-        {
-          variant: "white",
-          images: [
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
-          ],
-        },
-        {
-          variant: "black",
-          images: [
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
-          ],
-        },
-        {
-          variant: "green",
-          images: [
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
-            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
-          ],
-        },
-      ],
-    },
-    {
-      id: 11,
-      tittle: "Samsung Galaxy S23 Ultra 512 GB",
-      category: "Phone",
-      description: "Samsung Galaxy S23 Ultra 512 GB",
-      brand: "SAMSUNG",
-      color: ["white", "black", "green"],
-      memory: [256, 512],
-      rating: {
-        stars: 5,
-        count: 11,
-      },
-      features: {
-        ProcessorSpeed: 3.36,
-        NumberofProcessors: 8,
-        display: 6.8,
-        processor: "Qualcomm SM8550",
-        ram: 8,
-        camera: 200,
-      },
-      price: {
-        quantity: 41.499,
         currency: "TRY",
       },
       images: [
