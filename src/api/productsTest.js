a237cb878197441cbf466c96b736300ce571d5c0
ismailcubuk <iismailcubuk@gmail.com>
2023-05-30 12:47:51 +0300

    change camera mp

--- src/api/productsTest.js
+++ src/api/productsTest.js
@@ -1,8 +1,50 @@
 const products = {
-    phones: [
- 
-    ],
-  };
-  
-  export default products;
-  
+  phones: [
+    {
+      id: 5,
+      tittle: "iPhone 13 256 GB",
+      category: "Phone",
+      description: "iPhone 13 256 GB",
+      color: ["white", "red", "blue", "purple", "black", "green"],
+      rating: {
+        stars: 4,
+        count: 518,
+      },
+      features: {
+        ProcessorSpeed: null,
+        NumberofProcessors: 6,
+        display: "6.1",
+        processor: "A15 Bionic",
+        ram: 6,
+        camera: "12",
+      },
+      price: {
+        quantity: 32.799,
+        currency: "TRY",
+      },
+    },
+    {
+        id: 6,
+        tittle: "iPhone 13 512 GB",
+        category: "Phone",
+        description: "iPhone 13 512 GB",
+        color: ["white", "red", "blue", "purple", "black", "green"],
+        rating: {
+          stars: 4,
+          count: 518,
+        },
+        features: {
+          ProcessorSpeed: null,
+          NumberofProcessors: 6,
+          display: "6.1",
+          processor: "A15 Bionic",
+          ram: 6,
+          camera: "12",
+        },
+        price: {
+          quantity: 32.799,
+          currency: "TRY",
+        },
+      },
+  ],
+};
