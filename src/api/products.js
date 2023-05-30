3ecb3055804e78db9e1cd08ed18fc474d72bcefe
ismailcubuk <iismailcubuk@gmail.com>
2023-05-30 16:54:25 +0300

    Samsung Galaxy Z Fold 4

--- src/api/products.js
+++ src/api/products.js
@@ -736,7 +736,7 @@ const products = {
       description: "XİAOMİ POCO X4 GT 8GB/256GB",
       brand: "XIAOMI",
       color: ["white", "black"],
-      memory: 256,
+      memory: [256],
       rating: {
         stars: 3,
         count: 192,
@@ -781,7 +781,7 @@ const products = {
       description: "XİAOMİ POCO X5 8GB/256GB",
       brand: "XIAOMI",
       color: ["black"],
-      memory: 256,
+      memory: [256],
       rating: {
         stars: 3,
         count: 42,
@@ -817,7 +817,7 @@ const products = {
       description: "Xiaomi Redmi Note 12 6GB/128GB",
       brand: "XIAOMI",
       color: ["grey", "green", "blue"],
-      memory: 256,
+      memory: [256],
       rating: {
         stars: 3,
         count: 10,
@@ -1102,6 +1102,60 @@ const products = {
       },
       price: {
         quantity: 41.499,
+        currency: "TRY",
+      },
+      images: [
+        {
+          variant: "white",
+          images: [
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
+          ],
+        },
+        {
+          variant: "black",
+          images: [
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
+          ],
+        },
+        {
+          variant: "green",
+          images: [
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-2_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-3_large.jpg",
+            "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-4_large.jpg",
+          ],
+        },
+      ],
+    },
+    {
+      id: 11,
+      tittle: "Samsung Galaxy Z Fold 4",
+      category: "Phone",
+      description: "Samsung Galaxy Z Fold 4",
+      brand: "SAMSUNG",
+      color: ["white", "black", "green"],
+      memory: [256],
+      rating: {
+        stars: 5,
+        count: 7,
+      },
+      features: {
+        ProcessorSpeed: 3.32,
+        NumberofProcessors: 8,
+        display: 6.2,
+        processor: "Qualcomm SM8475",
+        ram: 12,
+        camera: 50,
+      },
+      price: {
+        quantity: 38.499,
         currency: "TRY",
       },
       images: [
